ea3f4db3de5aec7d27afdc9c2b7489f143aa53f3
oanhhkim <kimoanhh0103@gmail.com>
2023-06-16 21:14:44 +0700

    fix throwsException

--- src/main/java/com/java/spring/books/controller/HandleExceptionController.java
+++ src/main/java/com/java/spring/books/controller/HandleExceptionController.java
@@ -2,16 +2,19 @@ package com.java.spring.books.controller;
 
 import com.java.spring.books.exception.BaseException;
 import com.java.spring.books.exception.ExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class HandleExceptionController {
     @ExceptionHandler(BaseException.class)
-    public ExceptionResponse handleException(BaseException ex) {
+    public ResponseEntity<ExceptionResponse> handleException(BaseException ex) {
         ExceptionResponse exceptionResponse = new ExceptionResponse();
         exceptionResponse.setErrorCode(ex.getErrorCode());
         exceptionResponse.setStatus(ex.getStatus());
-        return exceptionResponse;
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.valueOf(ex.getErrorCode()));
     }
 }
